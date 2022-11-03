b5f065039efc996ee0b3b696d6c1cc0b14407c43
leonel05 <leonelvargas5012@gmail.com>
2022-11-03 16:13:19 -0500

    subiendo el proyecto

---
+++ view/Historial_clinico.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Historial Clinico </title>
+  </head>
+  <body>
+    <br>
+    <h1 class="text-center font-weight-bold mb-4">Historial Clinico</h1>
+    <section>
+    <div class="px-lg-5 py-lg-4 p-4">
+    <form method="POST" action="">
+        
+                        <div class="mb-3">
+                          <label for="exampleInputEmail1" class="form-label font-weight-bold">Nombre</label>
+                          <input Required type="Text" class="form-control bg-dark-x border-0" placeholder="Nombre del paciente" id="nombre" name="nombre" aria-describedby="nombre">                        
+                        </div>
+
+                        <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label font-weight-bold">Apellido</label>
+                        <input Required type="Text" class="form-control bg-dark-x border-0" placeholder="Apellido del paciente" id="apellido" name="apellido" aria-describedby="apellido">
+                        </div>
+
+                        <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label font-weight-bold">Edad</label>
+                        <input Required type="number" class="form-control bg-dark-x border-0" placeholder="Edad del paciente" id="edad" name="edad" aria-describedby="edad">
+                        </div>
+
+                        <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label font-weight-bold">telefono</label>
+                        <input Required type="number" class="form-control bg-dark-x border-0" placeholder="telefono del paciente" id="telefono" name="telefono" aria-describedby="telefono">
+                        </div>
+
+                        <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label font-weight-bold">estado de salud</label>
+                        <input Required type="Text" class="form-control bg-dark-x border-0" placeholder="estado de salud del paciente" id="estado de salud" name="estado de salud" aria-describedby="estado de salud">
+                        </div>
+
+                        <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label font-weight-bold">antecedentes familiares</label>
+                        <input Required type="Text" class="form-control bg-dark-x border-0" placeholder="antecedentes familiares del paciente" id="antecedentes familiares" name="antecedentes familiares" aria-describedby="antecedentes familiares">
+                        </div>
+                      
+                        <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label font-weight-bold">Examen Medico</label>
+                        <input Required type="Text" class="form-control bg-dark-x border-0" placeholder="Examen Medico" id="apellido" name="Examen Medico" aria-describedby="Examen Medico">
+                        </div>
+                      
+                        <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label font-weight-bold">Resultados del examen</label>
+                        <input Required type="Text" class="form-control bg-dark-x border-0" placeholder="Resultados del examen" id="Resultados del examen" name="Resultados del examen" aria-describedby="Resultados del examen">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Guardar datos</button>
+                      </form>
+
+                      </form>
+</div>
+    </section>
+    <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
